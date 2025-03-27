1d4be02303efcd9ac6a18998548039b8706cd6c9
thereaper0602 <2251012121quang@ou.edu.vn>
2025-03-27 22:28:42 +0700

    xong service cho cloudinary

--- src/main/java/com/hatecode/services/impl/CloundinaryServicesImpl.java
+++ src/main/java/com/hatecode/services/impl/CloundinaryServicesImpl.java
@@ -23,9 +23,9 @@ import javafx.stage.Stage;
  */
 public class CloundinaryServicesImpl implements CloundinaryServices {
 
-    private static final String CLOUD_NAME = "dg66aou8q";
-    private static final String API_KEY = "531349977128123";
-    private static final String API_SECRET = "hzmzBu_hLqWFLjWbn3n2JNa2yHA";
+    private static final String CLOUD_NAME = "";
+    private static final String API_KEY = "";
+    private static final String API_SECRET = "";
 
     private final Cloudinary cloudinary;
 
