ebb7caa24d5da96587cd5b61b618a1f9c4214a3c
yudhnaaaaa <hoanganhduy75@gmaill.com>
2025-04-26 15:31:45 +0700

    fix search by date in maintenance history

--- src/main/java/com/hatecode/config/AppConfig.java
+++ src/main/java/com/hatecode/config/AppConfig.java
@@ -2,6 +2,10 @@ package com.hatecode.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
+import java.time.LocalDate;
+
+import static com.hatecode.utils.FormatDate.DATE_FORMATTER;
+
 public class AppConfig {
     private static final Dotenv dotenv = Dotenv.load();
     public static final Integer PAGE_SIZE = 15;
@@ -15,4 +19,7 @@ public class AppConfig {
     public static final String DB_URL = dotenv.get("DB_URL");
     public static final String DB_USER = dotenv.get("DB_USER");
     public static final String DB_PASS = dotenv.get("DB_PASS");
+
+    public static final LocalDate MIN_DATE = LocalDate.parse("01/01/2020", DATE_FORMATTER);
+    public static final LocalDate MAX_DATE = LocalDate.now().plusYears(1);
 }
